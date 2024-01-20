9ea3ee6629c70daec24f2b3e6179495bf8422128
meAltf <alataf.dce.cs1@gmail.com>
2024-01-21 01:50:08 +0530

    created employeeRespository

---
+++ src/main/java/com/alataf/javaJunit/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.alataf.javaJunit.springboot.repository;
+
+import com.alataf.javaJunit.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
